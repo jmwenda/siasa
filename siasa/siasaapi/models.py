c7ad595ded5203eb5643d9167ea1403ed135eda8
Jude Mwenda <judemwenda@gmail.com>
2012-10-16 11:54:51 +0300

    changes to the models

--- siasa/siasaapi/models.py
+++ siasa/siasaapi/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.gis.db import models
+
 
 # Create your models here.
+class County(models.Model):
+    county = models.CharField("County",max_length=120)
+    geom = models.MultiPolygonField()
+
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return self.county
+
+    class Meta:
+        verbose_name_plural = "Counties"
+
+class Constituency(models.Model):
+    constituency = models.CharField("Constituency",max_length=120)
+    county =  models.ForeignKey(County)
+    population = models.CharField(max_length=20)
+    registeredvoters = models.CharField("Registered Voters",max_length=120)
+    year = models.CharField("Year",max_length=120)
+    def __unicode__(self):
+        return self.constituency
+
+class Election(models.Model):
+    election = models.CharField("Election",max_length=120)
+    year = models.CharField("Year",max_length=120)
+    def __unicode__(self):
+        return self.election
+
+class Party(models.Model):
+   party = models.CharField("Party Name",max_length=120)
+   year_formed = models.DateTimeField()
+   def __unicode__(self):
+        return party
+
+class Politician(models.Model):
+   surname = models.CharField("Surname",max_length=120)
+   othernames = models.CharField("Other Names", max_length=120)
+   dateofbirth = models.DateTimeField("Date of Birth")
+   brief = models.TextField("Brief")
+
